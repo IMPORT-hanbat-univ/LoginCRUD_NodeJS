a783a3b34d3a3d6d25d1c1021e447833a4f402e2
lm_ziihyun <97448029+limjiihyun@users.noreply.github.com>
2022-12-30 03:17:48 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+const mysqul=require('mysql')
+
+const db= mysql.createConnection({
+    host: "localhost",
+    user: 'root',
+    password :'root',
+    port:3306,
+    database:'dbs'
+})
+
+db.connect();
+module.exports=db;
